77e3f2e1e70d20ad74ad641b0ed7afd5259fa077
nirav2000 <nirav2000+github@gmail.com>
2026-09-16 21:10:27 +0100

    Add Firebase configuration hook

---
+++ firebase-config.js
@@ -0,0 +1,3 @@
+// Optional deployment-wide Firebase config. Paste the standard Firebase web config below.
+// Users can also enter the same config in Settings; it is stored in this browser only.
+window.SNAG_FIREBASE_CONFIG = null;
