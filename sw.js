d252d2dcb1a73b39a40576d7a8233b9d49dcc131
nirav2000 <nirav2000+github@gmail.com>
2026-09-21 13:25:29 +0100

    Refresh PWA after Firebase bootstrap fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='snag-v3';
+const CACHE='snag-v4';
 const ASSETS=['./','./index.html','./styles.css','./notes.css','./app.js','./notes.js','./firebase-config.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
