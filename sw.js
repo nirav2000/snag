217d7cdb47e86f9d6f3c989d4f80e7d41ba9abaa
nirav2000 <nirav2000+github@gmail.com>
2026-09-21 14:19:39 +0100

    Publish network-first PWA cache v9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='snag-v8';
-const BUILD='2026.09.21.1415';
+const CACHE='snag-v9';
+const BUILD='2026.09.21.1430';
 const ASSETS=['./','./index.html','./styles.css?v='+BUILD,'./notes.css?v='+BUILD,'./app.js?v='+BUILD,'./notes.js?v='+BUILD,'./firebase-config.js?v='+BUILD,'./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
