8ad9b069b69a2e206132edc274865c521858d963
nirav2000 <nirav2000+github@gmail.com>
2026-09-22 13:47:45 +0100

    Publish development cache v36

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='snag-dev-v35';
-const BUILD='2026.09.22.1645';
+const CACHE='snag-dev-v36';
+const BUILD='2026.09.22.1705';
 const STATIC=['./styles.css?v='+BUILD,'./notes.css?v='+BUILD,'./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
